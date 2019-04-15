4c96513ac5b96ec104531d81f836649b6da9bf9a
huzhiwu <1627983849@qq.com>
2019-04-15 15:47:02 +0800

    添加了音乐播放组件

--- pages/content/content.js
+++ pages/content/content.js
@@ -13,6 +13,13 @@ Page({
     ],
     articleDetail:{},
     poster:true,
+    playing:false,
+    currentTime:0,
+    currentPercent:0,
+    progressWidth:520,
+    percentCircleLeft:0,
+    audioCirleOriginX:0,
+    audioCircleMoveX:0,
   },
 
   /**
@@ -37,7 +44,7 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    
   },
   onplay:function(){
     let videoContext = wx.createVideoContext("video",this);
@@ -48,6 +55,128 @@ Page({
       poster:poster,
     })
     
+  },
+  audioPlay:function(){
+    let bgaudio = wx.getBackgroundAudioManager();
+    let audioData = this.data.articleDetail.audio;
+    bgaudio.src=audioData.src;
+    bgaudio.singer = audioData.singer;
+    bgaudio.title = audioData.title;
+    
+    this.listenAudioPlay();
+    this.updateAudioData();
+  },
+  onAudioPlay:function(){
+    let bgaudio = wx.getBackgroundAudioManager();
+    let playing = this.data.playing;
+    // 必须添加歌手的其他信息，单纯只有播放源会失败
+
+   
+    if(playing){
+      bgaudio.pause();
+    }else{
+      // let src = this.data.articleDetail.audio.src;
+      // bgaudio.src=src;
+      // bgaudio.singer = this.data.articleDetail.audio.singer;
+      // bgaudio.title = this.data.articleDetail.audio.title;
+      this.audioPlay()
+    }
+    this.setData({
+      playing:!playing,
+    })
+    // this.listenAudioPlay();
+    // this.updateAudioData();
+  },
+  listenAudioPlay:function(){
+    let that = this;
+    let bgaudio = wx.getBackgroundAudioManager();
+    bgaudio.onPause(function(){
+      that.setData({
+        playing:false,
+      })
+    })
+    bgaudio.onStop(function(){
+      that.setData({
+        playing:false,
+      })
+    })
+    bgaudio.onEnded(function(){
+      that.setData({
+        playing:false,
+      })
+    })
+    bgaudio.onPlay(function(){
+      that.setData({
+        playing:true,
+      })
+    })
+  },
+  updateAudioData:function(){
+    let bgaudio = wx.getBackgroundAudioManager();
+    let that = this;
+    let duration = this.data.articleDetail.audio.duration;
+    let width = this.data.progressWidth;
+    bgaudio.onTimeUpdate(function(){
+      let currentTime = bgaudio.currentTime;
+      let currentPercent = (currentTime/duration)*100
+
+      let percentCircleLeft = (currentTime/duration)*width
+      that.setData({ 
+        currentTime:bgaudio.currentTime.toFixed(),
+        currentPercent:currentPercent,
+        percentCircleLeft:percentCircleLeft,
+      })
+    })
+  },
+  onAudioCircleStart:function(e){
+    console.log(e.touches[0].pageX)
+    let audioCircleOriginX = (e.touches[0].pageX)*this.getPhoneRadio()
+    this.setData({
+      audioCircleOriginX:audioCircleOriginX,
+    })
+  },
+  onAudioCircleMove:function(e){
+   
+    
+    let moveX = e.touches[0].pageX*this.getPhoneRadio();
+    
+    let originX = this.data.audioCircleOriginX;
+    let duration = this.data.articleDetail.audio.duration;
+    let moveWidth = (moveX-originX);
+    console.log("moveWidth"+moveWidth)
+    let newPercentCircleLeft = moveWidth+this.data.audioCircleOriginX-84*this.getPhoneRadio();;
+   
+    if(newPercentCircleLeft<=0){
+      newPercentCircleLeft=0
+    }
+    if(newPercentCircleLeft>=this.data.progressWidth){
+      newPercentCircleLeft=this.data.progressWidth
+    }
+    this.setData({
+      percentCircleLeft:newPercentCircleLeft,
+      currentPercent:(newPercentCircleLeft/this.data.progressWidth)*100
+    })
+    let bgaudio = wx.getBackgroundAudioManager();
+    // let audioData = this.data.articleDetail.audio;
+    // bgaudio.src=audioData.src;
+    // bgaudio.singer = audioData.singer;
+    // bgaudio.title = audioData.title;
+    this.audioPlay();
+    bgaudio.seek(newPercentCircleLeft/this.data.progressWidth*duration);
+  //  this.updateAudioData();
+  
+   
+  },
+  // 获取手机的像素比例，因为etoucher[0].pageX是像素值
+  getPhoneRadio:function(){
+    let radio=0;
+    wx.getSystemInfo({
+      success:function(res){
+       radio = res.pixelRatio
+       console.log("宽度"+res.screenWidth)
+      }
+    })
+    return radio;
   },
   /**
    * 生命周期函数--监听页面显示
